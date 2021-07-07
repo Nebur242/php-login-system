17277380100503acee40716534938b1017ef1708
Ruben Merson Nzaou <ruben-n@dba.ma>
2021-07-07 09:54:56 +0100

    added config , footer , login and register page

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+    if(!defined('__CONFIG__')){
+        exit('You do not have config file');
+    }
+
+    //Our config is below
+    
+?>
--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-<?php echo "hello world !"; ?>
+<?php 
+
+    define('__CONFIG__' , true);
+    require_once("inc/config.php"); 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.7.0/css/uikit.min.css" integrity="sha512-GcujYvLvDEr0V+LbgkfmAMYFjcKGfuuDNhOyUhoeSMDcINpUFdcJaIlNYBxtQAIk3ehoH1/+x4o9KkFcH16a8Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   
+    <title>Document</title>
+</head>
+<body>
+
+<div class="uk-section uk-container">
+        <?php 
+            echo "Hello world !"; 
+            echo date('Y m d');
+        ?>
+
+        <p>
+            <a href="./login.php">Login</a>
+            <a href="./register.php">Register</a>
+        </p>
+  	
+  	</div>
+
+    <?php require_once('inc/footer.php') ?>
+</body>
+</html>
